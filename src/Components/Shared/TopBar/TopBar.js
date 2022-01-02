11b0604fd615e890e3098013290e95126f32e1a4
rahmanhamim <rahmanhamiminfo@gmail.com>
2022-01-03 00:00:08 +0600

    top bar done

--- src/Components/Shared/TopBar/TopBar.js
+++ src/Components/Shared/TopBar/TopBar.js
@@ -5,18 +5,19 @@ import { HiOutlineMail } from "react-icons/hi";
 
 const TopBar = () => {
  return (
-  <div className="container d-flex justify-content-between my-1 top-bar-text">
-   <div>
-    <p className="top-bar-text-left my-0">Welcome to SoloTrip</p>
-   </div>
-   <div className="d-flex top-bar-text-right">
-    <p className="my-0">
-     <FcCallback /> 123-12345678
-    </p>
-    <p className="ms-4 my-0">
-     {" "}
-     <HiOutlineMail /> contact@solotrip.com
-    </p>
+  <div className="container-fluid top-bar-border">
+   <div className="container d-none d-md-flex justify-content-between py-2 top-bar-text">
+    <div>
+     <p className="top-bar-text-left my-0">Welcome to SoloTrip</p>
+    </div>
+    <div className="d-flex top-bar-text-right">
+     <p className="my-0">
+      <FcCallback /> 123-12345678
+     </p>
+     <p className="ms-4 my-0">
+      <HiOutlineMail /> contact@solotrip.com
+     </p>
+    </div>
    </div>
   </div>
  );
