bf8d30d46e8f050287cfe6eb7bdb679161fe6373
rahmanhamim <rahmanhamiminfo@gmail.com>
2022-01-02 22:14:30 +0600

    nav update

--- src/Components/Shared/Navbar/Headers.js
+++ src/Components/Shared/Navbar/Headers.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import './Headers.css';
-import { NavLink } from 'react-router-dom';
-import logo from '../../../images/solotrip with white text.png';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Headers.css";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../images/logo-main.png";
 
 const Headers = () => {
-    return (
-        <>
-            <Navbar style={{fontFamily: 'var(--poppins-font)'}} bg="light" expand="lg">
-                <Container fluid>
-                    <Navbar.Brand><img src={logo} alt='' width="80px" height="70px" />
-                    <span className='headers-text'>Solo<span className='simple-text'>Trip</span></span>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            
-                        </Nav>
-                        <NavLink to="#action1" className='menu-text'>Home</NavLink>
-                        <NavLink to="#action2" className='menu-text'>About</NavLink>
-                        
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            </>
-    );
+ return (
+  <>
+   <Navbar bg="light" expand="lg">
+    <Container>
+     <Navbar.Brand href="#home">
+      <img src={logo} alt="" height="60px" />
+     </Navbar.Brand>
+     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto">
+       <Link to="/" className="navbar-link-custom">
+        HOME
+       </Link>
+       <Link to="/rooms" className="navbar-link-custom">
+        ROOMS
+       </Link>
+       <Link to="/about" className="navbar-link-custom">
+        ABOUT
+       </Link>
+       <Link to="/contactus" className="navbar-link-custom">
+        CONTACT US
+       </Link>
+      </Nav>
+     </Navbar.Collapse>
+     <Link to="/home">
+      <button className="btn-regular navbar-book-btn">Book Now</button>
+     </Link>
+    </Container>
+   </Navbar>
+  </>
+ );
 };
 
 export default Headers;
