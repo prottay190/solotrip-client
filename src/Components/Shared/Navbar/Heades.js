40c7a8089a5915ebe20646d86a13139383add043
unknown <aarnob2020@gmail.com>
2022-01-02 16:43:52 +0600

    navbar complete

---
+++ src/Components/Shared/Navbar/Heades.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Headers.css';
+import { NavLink } from 'react-router-dom';
+import logo from '../../../images/solotrip with white text.png';
+
+const Heades = () => {
+    return (
+        <>
+            <Navbar style={{fontFamily: 'var(--poppins-font)'}} bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand><img src={logo} alt='' width="80px" height="70px" />
+                    <span className='headers-text'>Solo<span className='simple-text'>Trip</span></span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            
+                        </Nav>
+                        <NavLink to="#action1" className='menu-text'>Home</NavLink>
+                        <NavLink to="#action2" className='menu-text'>About</NavLink>
+                        
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            </>
+    );
+};
+
+export default Heades;
