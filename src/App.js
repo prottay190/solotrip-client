3f43d95913d3466c183744e2bad712df76e65586
unknown <aarnob2020@gmail.com>
2022-01-02 17:58:28 +0600

    Not Found page complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home/Home";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src='https://miro.medium.com/max/1838/1*zE2qnVTJehut7B8P2aMn3A.gif?fbclid=IwAR09ZMEfvH_YChuMGTU3KuGWtTQUxQoKNv6pranNzAKD96wv40_hCN1sqOM' alt='' />    
+        </div>
+    );
+};
+
+export default NotFound;
