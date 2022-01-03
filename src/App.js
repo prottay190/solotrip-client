4a34bda9620276f8933ceee9e2b2241b672efffd
unknown <aarnob2020@gmail.com>
2022-01-03 22:33:29 +0600

    contactus page complete

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          
           <Route path="/rooms" element={<PrivateRoute>
             <Rooms />
           </PrivateRoute>}>
