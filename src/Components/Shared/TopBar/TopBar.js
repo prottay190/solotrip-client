89b0bceed9d4cba5f6fbe5bdd0f18f41e385f618
shihab <shumonshihab62@gmail.com>
2022-01-03 23:00:43 +0600

    update topbar 1.1

--- src/Components/Shared/TopBar/TopBar.js
+++ src/Components/Shared/TopBar/TopBar.js
@@ -7,36 +7,6 @@ import useAuth from "../../../hooks/useAuth";
 import { Container } from "react-bootstrap";
 
 const TopBar = () => {
-<<<<<<< HEAD
-    const {user, logOut} = useAuth();
- return (
-  <div className="container-fluid top-bar-border">
-   <div className="container d-none d-md-flex justify-content-between py-2 top-bar-text">
-    <div>
-     <p className="top-bar-text-left my-0">Welcome to SoloTrip</p>
-    </div>
-    <div className="d-flex top-bar-text-right">
-     <p className="my-0">
-      <FcCallback /> 123-12345678
-     </p>
-     <p className="ms-4 my-0">
-      <HiOutlineMail /> contact@solotrip.com
-     </p>
-    {
-        user?.email ? 
-        <NavLink className="d-none d-lg-block" to="/login">
-        <button onClick={logOut} className="btn-regular navbar-book-btn">SignOut</button>
-       </NavLink>
-        :
-         <NavLink className="d-none d-lg-block" to="/login">
-         <button className="btn-regular navbar-book-btn">SignIn</button>
-        </NavLink>
-    }
-    </div>
-   </div>
-  </div>
- );
-=======
   const { user, logOut } = useAuth();
   return (
     <Container className="topbar-container my-2 mb-1">
@@ -71,7 +41,6 @@ const TopBar = () => {
       </div>
     </Container>
   );
->>>>>>> topbar-update
 };
 
 export default TopBar;
