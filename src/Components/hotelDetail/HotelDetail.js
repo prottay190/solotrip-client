f7d4dbbdbc9bbcfb342d4963e95e719cbb2a96eb
shihab <shumonshihab62@gmail.com>
2022-01-04 18:44:40 +0600

    added order functionality

--- src/Components/hotelDetail/HotelDetail.js
+++ src/Components/hotelDetail/HotelDetail.js
@@ -1,40 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router";
 import "./HotelDetail.css";
+import Headers from "../Shared/Navbar/Headers";
 const HotelDetail = () => {
   const { id } = useParams();
+  console.log(id);
   const [singleHotel, setSingleHotel] = useState({});
   useEffect(() => {
     fetch(`http://localhost:5000/hotels/${id}`)
       .then((res) => res.json())
       .then((data) => setSingleHotel(data));
-  }, []);
-  const {
-    hotel_name,
-    description,
-    heading,
-    images,
-    location,
-    perfect_for,
-    perfect_reason,
-    popular_facilities,
-    price,
-    rating,
-  } = singleHotel;
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    arrows: false,
+  }, [id]);
+  const { hotel_name, description, heading, main_image, price } = singleHotel;
+  const onSubmit = () => {
+    let newData = { hotel_name, main_image, price, status: "pending" };
+    fetch("http://localhost:5000/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert("Order Placed successfully");
+        }
+      });
   };
   return (
-    <Container>
-      <h2>{hotel_name}</h2>
-    </Container>
+    <>
+      <Headers />
+      <Container className="hotel-detail-container mt-5">
+        <h2 className=" mb-5 ms-3">{hotel_name}</h2>
+        <div className="d-flex justify-content-center">
+          <img src={main_image} alt="" className="img-fluid" />
+        </div>
+        <Row className="mt-5" xs={1} md={2}>
+          <Col className="col-md-7">
+            <h3>{heading}</h3>
+            <p>{description}</p>
+          </Col>
+          <Col>
+            <p className="fw-bold" style={{ color: "var(--yellow-color)" }}>
+              Price: $ {price}
+            </p>
+            <button
+              className="btn-regular ms-0 mb-5 mt-3 navbar-book-btn"
+              onClick={() => {
+                onSubmit();
+              }}
+            >
+              Reserve
+            </button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
