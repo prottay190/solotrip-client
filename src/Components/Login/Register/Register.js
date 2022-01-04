b1d07e167f5ef8c90c5731279ef08ea929a707c7
unknown <aarnob2020@gmail.com>
2022-01-04 23:12:30 +0600

    update register page

--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -16,24 +16,33 @@ const Register = () => {
     e.preventDefault();
   };
 
-    const handleOnChange = e => {
-        const field = e.target.name;
-        const value = e.target.value;
-        const newLoginData = { ...logInData };
-        newLoginData[field] = value;
-        setLoginData(newLoginData);
-    }
-    return (
-      <>
-        <Headers></Headers>
-        <Container className="p-5">
-          <Row>
-            <Col lg={2}></Col>
-            <Col lg={8} sm={12} xs={12} className="register-container">
-              <div className="register-form">
-                <h2 className="register-text">Please Register</h2>
-                {!isLoading && (
-                  <>
+  const handleOnChange = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newLoginData = { ...logInData };
+    newLoginData[field] = value;
+    setLoginData(newLoginData);
+  };
+  return (
+    <>
+      <Headers></Headers>
+      <Container className="p-5">
+        <Row>
+          <Col lg={2}></Col>
+          <Col lg={8} sm={12} className="register-container">
+            <div className="register-form">
+              <h2 className="register-text">Please Register</h2>
+              {!isLoading && (
+                <>
+                  <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="name"
+                      onChange={handleOnChange}
+                      placeholder="Enter your Name"
+                    />
+                  </Form.Group>
                   <Form onSubmit={handleLoginSubmit}>
                     <Form.Group className="mb-3" controlId="formGroupEmail">
                       <Form.Label>Email address</Form.Label>
@@ -44,50 +53,29 @@ const Register = () => {
                         placeholder="Enter email"
                       />
                     </Form.Group>
-                  
-                      <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                          type="email"
-                          name="email"
-                          onChange={handleOnChange}
-                          placeholder="Enter email"
-                        />
-                      </Form.Group>
-                      <Form.Group
-                        className="mb-3"
-                        controlId="formGroupPassword"
-                      >
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          name="password"
-                          onChange={handleOnChange}
-                          placeholder="Password"
-                        />
-                      </Form.Group>
-                      <NavLink style={{ textDecoration: "none" }} to="/login">
-                        <span className="link-text">
-                          Already Registered? please LogIn
-                        </span>
-                      </NavLink> <br />
-                      <button className="lonin-btn" type="submit">
-                        Register
-                      </button>
-                      <br />
-                    </Form>
-                  </>
-                )}
-                <div className="wrapper">
-                  <div className="divider div-transparent div-dot"></div>
-                </div>
-                {isLoading && <Spinner animation="border" variant="info" />}
-                {user?.email && (
-                  <Alert style={{ marginTop: "10px" }} variant="success">
-                    successfuly Registger!
-                  </Alert>
-                )}
-                {authError && <Alert variant="danger">{authError}</Alert>}
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control
+                        type="password"
+                        name="password"
+                        onChange={handleOnChange}
+                        placeholder="Password"
+                      />
+                    </Form.Group>
+                    <NavLink style={{ textDecoration: "none" }} to="/login">
+                      <span className="link-text">
+                        Already Registered? please LogIn
+                      </span>
+                    </NavLink>
+                    <button className="lonin-btn" type="submit">
+                      Register
+                    </button>{" "}
+                    <br />
+                  </Form>
+                </>
+              )}
+              <div className="wrapper">
+                <div className="divider div-transparent div-dot"></div>
               </div>
               {isLoading && <Spinner animation="border" variant="info" />}
               {user?.email && (
@@ -96,6 +84,7 @@ const Register = () => {
                 </Alert>
               )}
               {authError && <Alert variant="danger">{authError}</Alert>}
+            </div>
           </Col>
           <Col lg={2}></Col>
         </Row>
